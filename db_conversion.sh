755021d746148cba87cddc7aba9532937e477d4a
Kai Teoh <kai.teoh@gmail.com>
2022-01-05 16:02:13 -0500

    run scripts before conversion

--- db_conversion.sh
+++ db_conversion.sh
@@ -2,6 +2,9 @@
 cd /www/scripts/railroads
 source env/bin/activate
 
+pip install -r requirements.txt
+python3 app.py
+
 for f in csv/*; do
 #   echo "File -> $f"
   echo $(echo $f | sed -e "s/^csv\///" -e 's/\.csv$//'); 
